1cefd360e962c1330b43eed441f19a983761693f
Huzaifa Javed Ansari <huzaifaansari@eng.rizvi.edu.in>
2024-11-30 12:07:32 +0530

    Add files via upload

--- Exp 6/task1.c
+++ Exp 6/task1.c
@@ -1,1 +1,62 @@
+/*
+    Expt. No : 6
+    Title : WAP to define a counter function to print how many times the function is called. use
+    storage classes.
 
+
+    Name: Ansari Huzaifa Javed Ahmed
+    Class & Div: ECS-E
+    UIN: 241S035
+ */
+
+
+ #include<stdio.h>
+extern int countExtern;
+void counterFunctionStatic() {
+static int countStatic = 0;
+countStatic++;
+printf("Static: Function called %d times\n", countStatic);
+}
+void counterFunctionAuto() {
+auto int countAuto = 0;
+countAuto++;
+printf("Auto: Function called %d times\n", countAuto);
+}
+void counterFunctionRegister() {
+register int countRegister = 0;
+countRegister++;
+printf("Register: Function called %d times\n", countRegister);
+}
+void counterFunctionExtern() {
+extern int countExtern;
+countExtern++;
+printf("Extern: Function called %d times\n", countExtern);
+}
+int countExtern = 0;
+int main() {
+counterFunctionStatic();
+counterFunctionStatic();
+counterFunctionStatic();
+counterFunctionAuto();
+counterFunctionAuto();
+counterFunctionRegister();
+counterFunctionRegister();
+counterFunctionExtern();
+counterFunctionExtern();
+counterFunctionExtern();
+return 0;
+}
+/* OUTPUT
+
+Static: Function called 1 times
+Static: Function called 2 times
+Static: Function called 3 times
+Auto: Function called 1 times
+Auto: Function called 1 times
+Register: Function called 1 times
+Register: Function called 1 times
+Extern: Function called 1 times
+Extern: Function called 2 times
+Extern: Function called 3 times
+
+*/
